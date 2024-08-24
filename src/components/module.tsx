4584ce780163cdad75e60026b215153fcaf2ec8b
devlucaslima <developerlucaslima@gmail.com>
2024-08-24 14:43:03 -0300

    ✨ component: module (added radix collapsible)

--- src/components/module.tsx
+++ src/components/module.tsx
@@ -1,5 +1,5 @@
-import { ChevronDown, ChevronUp } from 'lucide-react'
-import { useState } from 'react'
+import * as Collapsible from '@radix-ui/react-collapsible'
+import { ChevronDown } from 'lucide-react'
 
 import { Lesson } from './lesson'
 
@@ -10,18 +10,9 @@ interface ModuleProps {
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
-  const [isOpen, setIsOpen] = useState(false)
-
-  function toggleLessons() {
-    setIsOpen(!isOpen)
-  }
-
   return (
-    <div className="ease-in-out duration-300">
-      <button
-        onClick={toggleLessons}
-        className="flex w-full items-center gap-3 bg-zinc-800 p-4"
-      >
+    <Collapsible.Root className="group">
+      <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
         <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
           {moduleIndex + 1}
         </div>
@@ -33,20 +24,16 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
           </span>
         </div>
 
-        {isOpen ? (
-          <ChevronUp className="w-5 h-5 ml-auto text-zinc-400" />
-        ) : (
-          <ChevronDown className="w-5 h-5 ml-auto text-zinc-400" />
-        )}
-      </button>
+        <ChevronDown className="w-5 h-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
+      </Collapsible.Trigger>
 
-      {isOpen && (
-        <nav className="relative flex flex-col gap-4 p-6 ease-in-out duration-300">
+      <Collapsible.Content>
+        <nav className="relative flex flex-col gap-4 p-6">
           <Lesson title="Fundamentos do Redux" duration="09:13" />
           <Lesson title="Fundamentos do Redux" duration="09:13" />
           <Lesson title="Fundamentos do Redux" duration="09:13" />
         </nav>
-      )}
-    </div>
+      </Collapsible.Content>
+    </Collapsible.Root>
   )
 }
