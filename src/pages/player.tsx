7fb087e743df4796b29235afa0d445a7fc930663
devlucaslima <developerlucaslima@gmail.com>
2024-08-26 12:09:42 -0300

    ✨ page: player (setting current lesson as document title )

--- src/pages/player.tsx
+++ src/pages/player.tsx
@@ -1,43 +1,50 @@
 import { MessageCircle } from 'lucide-react'
+import { Helmet } from 'react-helmet-async'
 
 import { Header } from '../components/header'
 import { Module } from '../components/module'
 import { Video } from '../components/video'
+import { useCurrentLessonAndModule } from '../hooks/use-current-lesson-and-module'
 import { useAppSelector } from '../store'
 
 export function Player() {
+  const { currentLesson } = useCurrentLessonAndModule()
+
   const modules = useAppSelector((store) => store.player.course.modules)
 
   return (
-    <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
-      <div className="flex w-[1100px] flex-col gap-6">
-        <div className="flex items-center justify-between">
-          <Header />
+    <>
+      <Helmet title={currentLesson.title} />
+      <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
+        <div className="flex w-[1100px] flex-col gap-6">
+          <div className="flex items-center justify-between">
+            <Header />
 
-          <button className="flex items-center gap-2 rounded bg-violet-500 px-3 py-2 text-sm font-medium text-white hover:bg-violet-600">
-            <MessageCircle className="w-4 h-4" />
-            Leave feedback
-          </button>
-        </div>
-
-        <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow pr-80">
-          <div className="flex-1">
-            <Video />
+            <button className="flex items-center gap-2 rounded bg-violet-500 px-3 py-2 text-sm font-medium text-white hover:bg-violet-600">
+              <MessageCircle className="w-4 h-4" />
+              Leave feedback
+            </button>
           </div>
-          <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-auto scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
-            {modules.map((module, index) => {
-              return (
-                <Module
-                  key={module.id}
-                  moduleIndex={index}
-                  title={module.title}
-                  amountOfLessons={module.lessons.length}
-                />
-              )
-            })}
-          </aside>
-        </main>
+
+          <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow pr-80">
+            <div className="flex-1">
+              <Video />
+            </div>
+            <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-auto scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
+              {modules.map((module, index) => {
+                return (
+                  <Module
+                    key={module.id}
+                    moduleIndex={index}
+                    title={module.title}
+                    amountOfLessons={module.lessons.length}
+                  />
+                )
+              })}
+            </aside>
+          </main>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
