7c81f0540f620cfd5faa1e040b557f48b0e41cc9
devlucaslima <developerlucaslima@gmail.com>
2024-08-26 14:32:24 -0300

    ✨ hook: use lessons ( optional chaining "?" )

--- src/hooks/use-lessons.ts
+++ src/hooks/use-lessons.ts
@@ -2,7 +2,7 @@ import { useAppSelector } from '../store'
 
 export function useLessons(moduleIndex: number) {
   return useAppSelector((state) => {
-    const lessons = state.player.course.modules[moduleIndex].lessons
+    const lessons = state.player.course?.modules[moduleIndex].lessons
 
     return { lessons }
   })
