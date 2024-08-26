2bb781cc716fdcfbdfa23bab550a46133683fa92
devlucaslima <developerlucaslima@gmail.com>
2024-08-26 12:05:55 -0300

    ✨ component: header (receiving data from redux store player)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,13 @@
+import { useCurrentLessonAndModule } from '../hooks/use-current-lesson-and-module'
+
 export function Header() {
+  const { currentModule, currentLesson } = useCurrentLessonAndModule()
+
   return (
     <div className="flex flex-col gap-1">
-      <h1 className="text-2xl font-bold">Redux fundamentals</h1>
+      <h1 className="text-2xl font-bold">{currentLesson.title}</h1>
       <span className="text-sm text-zinc-400">
-        Module: Unraveling the Redux
+        Module: {currentModule.title}
       </span>
     </div>
   )
