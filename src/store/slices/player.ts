264bf1c4bb992b80180e0470c8e2652778fd1bad
devlucaslima <developerlucaslima@gmail.com>
2024-08-26 12:11:24 -0300

    ✨ redux slice: player (added reducers to set next video automatically)

--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 const playerSlice = createSlice({
   name: 'player',
@@ -7,78 +7,95 @@ const playerSlice = createSlice({
       modules: [
         {
           id: '1',
-          title: 'Iniciando com Redux',
+          title: 'Getting Started with Redux',
           lessons: [
             {
               id: 'Jai8w6K_GnY',
-              title: 'Introdução ao Redux',
+              title: 'Introduction to Redux',
               duration: '13:45',
             },
             {
               id: 'w-DW4DhDfcw',
-              title: 'Configurando Redux no Projeto',
+              title: 'Setting Up Redux in the Project',
               duration: '10:05',
             },
             {
               id: 'D83-55LUdKE',
-              title: 'Entendendo Actions e Reducers',
+              title: 'Understanding Actions and Reducers',
               duration: '06:33',
             },
             {
               id: 'W_ATsETujaY',
-              title: 'Integração com Redux DevTools',
+              title: 'Integration with Redux DevTools',
               duration: '09:12',
             },
             {
               id: 'Pj8dPeameYo',
-              title: 'Middleware no Redux',
+              title: 'Middleware in Redux',
               duration: '03:23',
             },
             {
               id: '8KBq2vhwbac',
-              title: 'Async Actions com Redux Thunk',
+              title: 'Async Actions with Redux Thunk',
               duration: '11:34',
             },
           ],
         },
         {
           id: '2',
-          title: 'Estrutura Avançada com Zustand',
+          title: 'Advanced Structure with Zustand',
           lessons: [
             {
               id: 'gE48FQXRZ_o',
-              title: 'Introdução ao Zustand',
+              title: 'Introduction to Zustand',
               duration: '13:45',
             },
             {
               id: 'Ng_Vk4tBl0g',
-              title: 'Criando e Usando Stores no Zustand',
+              title: 'Creating and Using Stores in Zustand',
               duration: '10:05',
             },
             {
               id: 'h5JA3wfuW1k',
-              title: 'Utilizando Middleware no Zustand',
+              title: 'Using Middleware in Zustand',
               duration: '06:33',
             },
             {
               id: '1G0vSTqWELg',
-              title: 'Persistência de Estado com Zustand',
+              title: 'State Persistence with Zustand',
               duration: '09:12',
             },
           ],
         },
       ],
     },
-    currentLessonIndex: 0,
     currentModuleIndex: 0,
+    currentLessonIndex: 0,
   },
   reducers: {
-    play: (state, action) => {
-      state.currentLessonIndex = action.payload[0]
-      state.currentModuleIndex = action.payload[1]
+    play: (state, action: PayloadAction<[number, number]>) => {
+      state.currentModuleIndex = action.payload[0]
+      state.currentLessonIndex = action.payload[1]
+    },
+    next: (state) => {
+      const nextLessonIndex = state.currentLessonIndex + 1
+      const nextLesson =
+        state.course.modules[state.currentModuleIndex].lessons[nextLessonIndex]
+
+      if (nextLesson) {
+        state.currentLessonIndex = nextLessonIndex
+      } else {
+        const nextModuleIndex = state.currentModuleIndex + 1
+        const nextModule = state.course.modules[nextModuleIndex]
+
+        if (nextModule) {
+          state.currentModuleIndex = nextModuleIndex
+          state.currentLessonIndex = 0
+        }
+      }
     },
   },
 })
 
 export const player = playerSlice.reducer
-export const { play } = playerSlice.actions
+export const { play, next } = playerSlice.actions
