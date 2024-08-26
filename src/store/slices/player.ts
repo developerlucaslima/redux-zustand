aa0d4d987161b4ce4ba248f2415932d83bd0c9f6
devlucaslima <developerlucaslima@gmail.com>
2024-08-26 14:34:38 -0300

    ✨ redux slice: player ( get initial data from db and tying properties )

--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -1,78 +1,37 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
+interface Course {
+  id: number
+  modules: Array<{
+    id: number
+    title: string
+    lessons: Array<{
+      id: string
+      title: string
+      duration: string
+    }>
+  }>
+}
+
+interface PlayerState {
+  course: Course | null
+  currentModuleIndex: number
+  currentLessonIndex: number
+}
+
+const initialState: PlayerState = {
+  course: null,
+  currentModuleIndex: 0,
+  currentLessonIndex: 0,
+}
+
 const playerSlice = createSlice({
   name: 'player',
-  initialState: {
-    course: {
-      modules: [
-        {
-          id: '1',
-          title: 'Getting Started with Redux',
-          lessons: [
-            {
-              id: 'Jai8w6K_GnY',
-              title: 'Introduction to Redux',
-              duration: '13:45',
-            },
-            {
-              id: 'w-DW4DhDfcw',
-              title: 'Setting Up Redux in the Project',
-              duration: '10:05',
-            },
-            {
-              id: 'D83-55LUdKE',
-              title: 'Understanding Actions and Reducers',
-              duration: '06:33',
-            },
-            {
-              id: 'W_ATsETujaY',
-              title: 'Integration with Redux DevTools',
-              duration: '09:12',
-            },
-            {
-              id: 'Pj8dPeameYo',
-              title: 'Middleware in Redux',
-              duration: '03:23',
-            },
-            {
-              id: '8KBq2vhwbac',
-              title: 'Async Actions with Redux Thunk',
-              duration: '11:34',
-            },
-          ],
-        },
-        {
-          id: '2',
-          title: 'Advanced Structure with Zustand',
-          lessons: [
-            {
-              id: 'gE48FQXRZ_o',
-              title: 'Introduction to Zustand',
-              duration: '13:45',
-            },
-            {
-              id: 'Ng_Vk4tBl0g',
-              title: 'Creating and Using Stores in Zustand',
-              duration: '10:05',
-            },
-            {
-              id: 'h5JA3wfuW1k',
-              title: 'Using Middleware in Zustand',
-              duration: '06:33',
-            },
-            {
-              id: '1G0vSTqWELg',
-              title: 'State Persistence with Zustand',
-              duration: '09:12',
-            },
-          ],
-        },
-      ],
-    },
-    currentModuleIndex: 0,
-    currentLessonIndex: 0,
-  },
+  initialState,
   reducers: {
+    start: (state, action: PayloadAction<Course>) => {
+      state.course = action.payload
+    },
     play: (state, action: PayloadAction<[number, number]>) => {
       state.currentModuleIndex = action.payload[0]
       state.currentLessonIndex = action.payload[1]
@@ -80,13 +39,13 @@ const playerSlice = createSlice({
     next: (state) => {
       const nextLessonIndex = state.currentLessonIndex + 1
       const nextLesson =
-        state.course.modules[state.currentModuleIndex].lessons[nextLessonIndex]
+        state.course?.modules[state.currentModuleIndex].lessons[nextLessonIndex]
 
       if (nextLesson) {
         state.currentLessonIndex = nextLessonIndex
       } else {
         const nextModuleIndex = state.currentModuleIndex + 1
-        const nextModule = state.course.modules[nextModuleIndex]
+        const nextModule = state.course?.modules[nextModuleIndex]
 
         if (nextModule) {
           state.currentModuleIndex = nextModuleIndex
@@ -98,4 +57,4 @@ const playerSlice = createSlice({
 })
 
 export const player = playerSlice.reducer
-export const { play, next } = playerSlice.actions
+export const { start, play, next } = playerSlice.actions
