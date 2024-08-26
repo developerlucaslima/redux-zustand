502a4ff7487b714499e8f004199bfc0ce7fca45a
devlucaslima <developerlucaslima@gmail.com>
2024-08-26 14:30:49 -0300

    ✨ page: player ( optional chaining "?" and get data from db )

--- src/pages/player.tsx
+++ src/pages/player.tsx
@@ -1,20 +1,31 @@
 import { MessageCircle } from 'lucide-react'
+import { useEffect } from 'react'
 import { Helmet } from 'react-helmet-async'
+import { useDispatch } from 'react-redux'
 
 import { Header } from '../components/header'
 import { Module } from '../components/module'
 import { Video } from '../components/video'
 import { useCurrentLessonAndModule } from '../hooks/use-current-lesson-and-module'
+import { api } from '../lib/axios'
 import { useAppSelector } from '../store'
+import { start } from '../store/slices/player'
 
 export function Player() {
+  const dispatch = useDispatch()
   const { currentLesson } = useCurrentLessonAndModule()
 
-  const modules = useAppSelector((store) => store.player.course.modules)
+  const modules = useAppSelector((store) => store.player.course?.modules)
+
+  useEffect(() => {
+    api.get(`/course/1`).then((response) => {
+      dispatch(start(response.data))
+    })
+  })
 
   return (
     <>
-      <Helmet title={currentLesson.title} />
+      <Helmet title={currentLesson?.title} />
       <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
         <div className="flex w-[1100px] flex-col gap-6">
           <div className="flex items-center justify-between">
@@ -31,16 +42,17 @@ export function Player() {
               <Video />
             </div>
             <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-auto scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
-              {modules.map((module, index) => {
-                return (
-                  <Module
-                    key={module.id}
-                    moduleIndex={index}
-                    title={module.title}
-                    amountOfLessons={module.lessons.length}
-                  />
-                )
-              })}
+              {modules &&
+                modules.map((module, index) => {
+                  return (
+                    <Module
+                      key={module.id}
+                      moduleIndex={index}
+                      title={module.title}
+                      amountOfLessons={module.lessons.length}
+                    />
+                  )
+                })}
             </aside>
           </main>
         </div>
